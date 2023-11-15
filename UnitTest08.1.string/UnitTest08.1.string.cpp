4a7052ae824994ad2f691199e09b1db352c103d8
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-11-15 14:07:42 +0200

    second test

---
+++ UnitTest08.1.string/UnitTest08.1.string.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR-08.1.string/PR-08.1.string.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest081string
+{
+	TEST_CLASS(UnitTest081string)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			{
+				char str[101] = "a**b**c**d";
+
+				int pos = Count(str);
+
+				Assert::AreEqual(pos, 0);
+			}
+
+		}
+	};
+}
